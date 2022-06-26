f7e2a024fa4ba0feb74cad1820e46ac28d7db0bb
OnurOgur <80548120+OnurOgur@users.noreply.github.com>
2022-06-26 21:20:30 +0300

    Blog adding dynamically.

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const path = require("path");
+const Schema = mongoose.Schema;
+const Blog = require("./models/Blog");
 
 const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/cleanblog-test-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const port = 3200;
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/", async(req, res) => {
+    const blogs = await Blog.find({});
+    res.render("index", { blogs });
 });
 
 app.get("/about", (req, res) => {
@@ -24,6 +35,11 @@ app.get("/add_post", (req, res) => {
 
 app.get("/post", (req, res) => {
     res.render("post");
+});
+
+app.post("/blogs", async(req, res) => {
+    await Blog.create(req.body);
+    res.redirect("/");
 });
 
 app.listen(port, () => {
